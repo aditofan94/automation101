f99306a053a63e64970aabe44de5d33b9f0fe307
Adi Tofan <adi_tofan@yahoo.com>
2021-03-20 15:05:16 +0200

    update 1

--- src/test/java/org/testCase1.java
+++ src/test/java/org/testCase1.java
@@ -1,6 +1,8 @@
 package org;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,8 +12,9 @@ public class testCase1 {
     public void testCase1 () {
         System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("http://google.com");
+        driver.get("http://emag.ro");
         driver.manage().window().maximize();
+        driver.findElement(By.id("searchboxTrigger")).sendKeys("mouse gaming" + Keys.ENTER);
 
     }
 }
--- src/test/java/org/testCase2.java
+++ src/test/java/org/testCase2.java
@@ -1,4 +1,20 @@
 package org;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class testCase2 {
+
+    @Test
+    public void testCase2 () {
+        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://emag.ro");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("searchboxTrigger")).sendKeys("!@#$%" + Keys.ENTER);
+
+    }
 }
